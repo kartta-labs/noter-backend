8684bd9c1324f1f3030a84090202f3665407854b
Sasan Tavakkol <sasantv@google.com>
2020-07-08 03:28:02 +0000

    remove unused csrf_exempt

--- noter_backend/noter_backend/urls.py
+++ noter_backend/noter_backend/urls.py
@@ -4,13 +4,12 @@ from main import views
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
     path('api/v0.1/images/', views.ImageList.as_view()),
     path('api/v0.1/images/<int:pk>/', views.ImageDetail.as_view()),
-    path('api/v0.1/projects/', csrf_exempt(views.ProjectList.as_view())),
+    path('api/v0.1/projects/', views.ProjectList.as_view()),
     path('api/v0.1/projects/<int:pk>/', views.ProjectDetail.as_view()),
     path('api/v0.1/annotations/', views.AnnotationsJsonList.as_view()),
     path('api/v0.1/annotations/<int:pk>/', views.AnnotationsJsonDetail.as_view()),
