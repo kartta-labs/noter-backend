c9ee9bf27e4581be4bbebcaf5a97fd9534a3b115
bladehan2020 <bladehan@google.com>
2020-09-24 16:32:10 -0400

    update entry point script to create the expected database on the fly

--- docker-compose-entrypoint.sh
+++ docker-compose-entrypoint.sh
@@ -4,6 +4,8 @@ until psql $DATABASE_URL -c '\l'; do
     >&2 echo "Posgres is unavailable - sleeping."
 done
 
+psql $DATABASE_URL -c "create database ${NOTER_DB_NAME}"
+
 python3 /noter-backend/noter_backend/manage.py makemigrations
 python3 /noter-backend/noter_backend/manage.py migrate
 python3 /noter-backend/noter_backend/manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'dontusethispassword')"
