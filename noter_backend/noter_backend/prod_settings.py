f9bd65e751cd78cb1a23d2af8b06b7b6c5ff7dda
Mark Phillips <embeepea@google.com>
2020-12-02 20:34:36 +0000

    add real prod_settings.py

--- noter_backend/noter_backend/prod_settings.py
+++ noter_backend/noter_backend/prod_settings.py
@@ -13,13 +13,31 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-raise Exception("Not implemented.")
-
 from noter_backend.settings import *
+from distutils.util import strtobool
+import sys
 
-DEBUG = False
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('NOTER_BACKEND_SECRET_KEY', 'n%n&-x^t7)60331uvy#stf-s20dvxd*m2^m3bvwjfh!m=11=sb')
+print('SECRET_KEY: {}'.format(SECRET_KEY))
 
+# SECURITY WARNING: don't run with debug turned on in production!
+NOTER_BACKEND_DEBUG = os.environ.get('NOTER_BACKEND_DEBUG', False)
+if isinstance(NOTER_BACKEND_DEBUG, str):
+  print('NOTER_BACKEND_DEBUG was a string')
+  NOTER_BACKEND_DEBUG = bool(strtobool(NOTER_BACKEND_DEBUG))
+DEBUG = NOTER_BACKEND_DEBUG
+print('DEBUG: {}'.format(DEBUG))
 
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+MIDDLEWARE.insert(0, 'main.middleware.DevXEmailMiddleware')
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'main.authentication.NoCsrfSessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
+    )
+}
+
+sys.stdout.flush()
